8c854436f55fa32692beeb2f44b47da90ac64418
namanh2310 <101866057+namanh2310@users.noreply.github.com>
2023-05-31 17:21:39 +0700

    test 2
    - Bar chart
      + UI/UX
      + Buttons

--- src/components/content/Content.jsx
+++ src/components/content/Content.jsx
@@ -1,11 +1,26 @@
-import React from 'react';
+import { useState } from 'react';
 import './Content.css';
 import Chart from './chart/Chart';
 
 const Content = () => {
+  const [type, setType] = useState('bar')
   return (
+
     <div className="content">
-      <Chart />
+      <div className='chart-title'><h1>Project Data Science and Visualization</h1></div>
+      <div style={{ display: 'flex', marginTop: '2rem' }}>
+        <div className='button-container'>
+          <button onClick={() => setType('bar')}>Test</button>
+          <button onClick={() => setType('test1')}>Test</button>
+          <button onClick={() => setType('test2')}>Test</button>
+          <button onClick={() => setType('test3')}>Test</button>
+        </div>
+        <div className='interactive-view'>
+
+          {type === 'bar' && <Chart type={type} />}
+          {type === 'test1' && <div style={{ display: 'flex', flexWrap: 'wrap' }}><h1>cailon</h1><h1>cailon</h1><h1>cailon</h1><h1>cailon</h1><h1>cailon</h1><h1>cailon</h1><h1>cailon</h1><h1>cailon</h1><h1>cailon</h1><h1>cailon</h1><h1>cailon</h1><h1>cailon</h1><h1>cailon</h1><h1>cailon</h1><h1>cailon</h1><h1>cailon</h1><h1>cailon</h1><h1>cailon</h1><h1>cailon</h1><h1>cailon</h1><h1>cailon</h1><h1>cailon</h1><h1>cailon</h1><h1>cailon</h1><h1>cailon</h1><h1>cailon</h1><h1>cailon</h1><h1>cailon</h1><h1>cailon</h1><h1>cailon</h1><h1>cailon</h1><h1>cailon</h1><h1>cailon</h1><h1>cailon</h1><h1>cailon</h1><h1>cailon</h1><h1>cailon</h1><h1>cailon</h1><h1>cailon</h1><h1>cailon</h1><h1>cailon</h1><h1>cailon</h1><h1>cailon</h1><h1>cailon</h1><h1>cailon</h1><h1>cailon</h1><h1>cailon</h1><h1>cailon</h1><h1>cailon</h1><h1>cailon</h1><h1>cailon</h1><h1>cailon</h1><h1>cailon</h1><h1>cailon</h1><h1>cailon</h1><h1>cailon</h1><h1>cailon</h1><h1>cailon</h1><h1>cailon</h1><h1>cailon</h1><h1>cailon</h1><h1>cailon</h1><h1>cailon</h1><h1>cailon</h1><h1>cailon</h1><h1>cailon</h1><h1>cailon</h1><h1>cailon</h1><h1>cailon</h1><h1>cailon</h1><h1>cailon</h1><h1>cailon</h1><h1>cailon</h1><h1>cailon</h1><h1>cailon</h1><h1>cailon</h1><h1>cailon</h1><h1>cailon</h1><h1>cailon</h1><h1>cailon</h1><h1>cailon</h1><h1>cailon</h1><h1>cailon</h1><h1>cailon</h1><h1>cailon</h1><h1>cailon</h1><h1>cailon</h1><h1>cailon</h1><h1>cailon</h1><h1>cailon</h1><h1>cailon</h1><h1>cailon</h1><h1>cailon</h1><h1>cailon</h1><h1>cailon</h1><h1>cailon</h1><h1>cailon</h1><h1>cailon</h1><h1>cailon</h1><h1>cailon</h1><h1>cailon</h1><h1>cailon</h1><h1>cailon</h1><h1>cailon</h1><h1>cailon</h1><h1>cailon</h1><h1>cailon</h1><h1>cailon</h1><h1>cailon</h1><h1>cailon</h1></div>}
+        </div>
+      </div>
     </div>
   );
 };
--- src/components/content/chart/Chart.jsx
+++ src/components/content/chart/Chart.jsx
@@ -1,33 +1,78 @@
 import React, { useEffect, useState } from "react";
 import * as d3 from "d3";
-import "./Chart.css"
+import "./Chart.css";
 
-const Chart = () => {
+const Chart = ({ type }) => {
   const [dataFetched, setDataFetched] = useState(false);
 
   useEffect(() => {
     const fetchData = async () => {
-      if (!dataFetched) {
+      if (true) {
         const response = await d3.csv(
-          "https://res.cloudinary.com/nguyenle23/raw/upload/v1685502289/me/fifa_data.csv"
+          "https://res.cloudinary.com/nguyenle23/raw/upload/v1685516151/me/fifa_500_new.csv"
         );
-        const data = response.map((d) => d.Age);
+
+        const countPlayersByCountry = (data) => {
+          const result = [];
+          for (let i = 0; i < data.length; i++) {
+            const country = data[i].Country;
+            const index = result.findIndex((item) => item.country === country);
+            if (index === -1) {
+              result.push({ country: country, count: 1 });
+            }
+            if (index !== -1) {
+              result[index].count += 1;
+            }
+          }
+          return result;
+        };
+
+        const data = countPlayersByCountry(response);
+        const scale_value = 2.5;
+        const chart_frame_height = 500;
+        const rect_width = 18.5;
+        const distance_next_rect = 20;
+        const padding_x = 128;
+
+
+        data.pop()
+        console.log(data)
+
+
+
+        const xScale = d3
+          .scaleBand()
+          .domain(data.map(d => d.country))
+          .range([0, data.length * distance_next_rect * scale_value])
+          .padding(0.1)
+        const xAxis = d3.axisBottom(xScale);
+
+        const yScale = d3
+          .scaleLinear()
+          .domain([58, 0])
+          .range([0, 290]);
+        const yAxis = d3.axisLeft(yScale);
 
         const svg = d3
           .select("#chart")
           .append("svg")
-          .attr("width", 1500)
-          .attr("height", 500);
+          .attr("width", data.length * distance_next_rect * scale_value + padding_x)
+          .attr("height", chart_frame_height)
+          .append("g");
 
         svg
           .selectAll("rect")
           .data(data)
           .enter()
           .append("rect")
-          .attr("x", (d, i) => i * 20)
-          .attr("y", (d) => 500 - d * 5)
-          .attr("width", 10)
-          .attr("height", (d) => d * 5)
+          .attr("x", (d, i) => i * distance_next_rect * scale_value + 50)
+          .attr("y", (d) => {
+            return chart_frame_height - d["count"] * 5 - distance_next_rect;
+          })
+          .attr("width", rect_width * scale_value)
+          .attr("height", (d) => {
+            return d["count"] * 5;
+          })
           .attr("fill", "white");
 
         svg
@@ -35,10 +80,50 @@ const Chart = () => {
           .data(data)
           .enter()
           .append("text")
-          .text((d) => d)
-          .attr("x", (d, i) => i * 20 - 2)
-          .attr("y", (d, i) => 500 - 5 * d - 3)
+          .text((d) => d["count"])
+          .attr("class", "text")
+          .attr("x", (d, i) => i * distance_next_rect * scale_value + 50 + (rect_width * scale_value) / 2)
+          .attr("y", (d, i) => {
+            return chart_frame_height - d["count"] * 5 - distance_next_rect * scale_value + 20;
+          })
           .attr("fill", "white");
+
+
+        svg
+          .append("g")
+          .attr("class", "x-axis")
+          .attr('transform', `translate(50, 480)`)
+          .call(xAxis);
+
+        svg
+          .append("text")
+          .attr("text-anchor", "end")
+          .attr("font-family", "sans-serif")
+          .attr("font-size", "15px")
+          .attr("font-weight", "bolder")
+          .attr("fill", "white")
+          .attr("x", data.length * distance_next_rect * scale_value + padding_x)
+          .attr("y", 480)
+          .attr("class", "countries")
+          .text("Countries");
+
+        svg
+          .append("g")
+          .attr("class", "y-axis")
+          .attr('transform', `translate(50, 190)`)
+          .call(yAxis);
+
+        svg
+          .append("text")
+          .attr("text-anchor", "end")
+          .attr("font-family", "sans-serif")
+          .attr("font-size", "15px")
+          .attr("font-weight", "bolder")
+          .attr("fill", "white")
+          .attr("x", 150)
+          .attr("y", 180)
+          .attr("class", "number")
+          .text("Number of players");
 
         setDataFetched(true);
       }
@@ -48,13 +133,15 @@ const Chart = () => {
     return () => {
       d3.select("#chart").selectAll("*").remove();
     };
-  }, [dataFetched]);
+  });
 
   return (
     <div id="chart-frame">
-      <div id="chart"></div>
+      <h2 className="title">Bar Chart Indicating Global Distribution of Football Players by Country</h2>
+      <div id="chart">
+      </div>
     </div>
-  )
+  );
 };
 
-export default Chart;
+export default Chart;   
